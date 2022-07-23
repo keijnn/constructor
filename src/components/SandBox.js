5a5f5190b2de17067f03d2036a3b4f84ff56899d
keijnn <kejnnn.zxc@gmail.com>
2022-07-23 09:43:33 +0300

    created full aplication logic

--- src/components/SandBox.js
+++ src/components/SandBox.js
@@ -26,25 +26,35 @@ export const SandBox = () => {
     if (item.title === 'Paragraph') {
       return (
         <div
-          className="w-9/12 whitespace-pre cursor-pointer mb-3 h-20 flex items-center justify-center text-[#97AACD]"
           key={item.id}
+          class="w-full cursor-pointer mb-3 text-center text-[#97AACD]"
         >
-          {item.value}
+          <div class="whitespace-normal">{item.value}</div>
+        </div>
+      )
+    } 
+    if (item.title === 'Button') {
+      return (
+        <div
+          key={item.id}
+          class="w-full  mb-3 flex items-center justify-center"
+        >
+          <div class="cursor-pointer flex items-center justify-center bg-[#4368E0] w-24 h-12 text-white">
+            {item.value}
+          </div>
         </div>
       )
     } 
 
     return (
-      <div
-        className="w-full cursor-pointer mb-3 rounded-xl h-20 flex flex-col items-center justify-center bg-[#D9E7FF]"
-        key={item.id}
-      >
+      <div className="w-full cursor-pointer flex justify-center items-center mb-3" key={item.id}>
+        <img src={item.value} alt='img' />
       </div>
     )
   })
 
   return (
-    <div className="pt-8 max-h-full overflow-auto px-5 col-start-4 p-3 col-end-7 row-span-6">
+    <div className="pt-8 max-w-screen max-h-full overflow-auto px-5 col-start-4 p-3 col-end-7 row-span-6">
       {sandboxField}
     </div>
   )
